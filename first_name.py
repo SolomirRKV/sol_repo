f421f331c51707fed0c1e230a78c4833dc7f78fd
Solomir <solomir000@gmail.com>
2018-11-24 17:01:24 +0300

    First add files

---
+++ first_name.py
@@ -0,0 +1,2 @@
+user_info = {"first_name" : "Kirill", "last_name" : "Rybakov"}
+print(user_info["first_name"])
---
+++ vat.py
@@ -0,0 +1,10 @@
+price = 100
+vat_rate = 18
+
+vat = price / 100 * vat_rate
+
+print (vat)
+
+price_no_vat = price -vat
+
+print('price_no_vat', price_no_vat)
